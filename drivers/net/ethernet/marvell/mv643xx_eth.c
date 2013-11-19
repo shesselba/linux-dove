ddbd204a6998257742e4e4a79c8a1e6bbf6a6253
Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
2013-11-19 19:08:34 +0100

    net: mv643xx_eth: resume/suspend PHY on port start/stop
    Network PHYs consume a noticable amount of power. This adds phy_resume
    on port start and phy_suspend on port stop to save this power if the
    port is down anyway. While at it, also properly start/stop the phy.
    
    Signed-off-by: Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>

--- drivers/net/ethernet/marvell/mv643xx_eth.c
+++ drivers/net/ethernet/marvell/mv643xx_eth.c
@@ -2097,6 +2097,8 @@ static void port_start(struct mv643xx_eth_private *mp)
 
 		mv643xx_eth_get_settings(mp->dev, &cmd);
 		phy_reset(mp);
+		phy_resume(mp->phy);
+		phy_start(mp->phy);
 		mv643xx_eth_set_settings(mp->dev, &cmd);
 	}
 
@@ -2305,6 +2307,11 @@ static int mv643xx_eth_stop(struct net_device *dev)
 		rxq_deinit(mp->rxq + i);
 	for (i = 0; i < mp->txq_count; i++)
 		txq_deinit(mp->txq + i);
+
+	if (mp->phy) {
+		phy_stop(mp->phy);
+		phy_suspend(mp->phy);
+	}
 
 	return 0;
 }
